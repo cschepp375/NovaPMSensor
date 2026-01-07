b6a229ca2d8b5f29733ed2241e418a9a7291e7b0
cschepp375 <dev@schepp.it>
2026-01-07 11:01:56 +0100

    Initial commit

---
+++ littleserver.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+// JSON-Body lesen
+app.use(express.json());
+
+// Alle Requests loggen
+app.use((req, res, next) => {
+  console.log("---- Incoming Request ----");
+  console.log("Method:", req.method);
+  console.log("URL:", req.originalUrl);
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
+  console.log("--------------------------");
+  next();
+});
+
+// Test-Endpunkt
+app.all('/dummy', (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
